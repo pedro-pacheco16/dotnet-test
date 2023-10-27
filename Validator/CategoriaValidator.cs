e77714fc9e5c240ed4d399cc8dc49e3286279d1f
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-10-26 23:40:10 -0300

    Criação da Validator Categoria

---
+++ Validator/CategoriaValidator.cs
@@ -0,0 +1,16 @@
+﻿using dotnet_test.Model;
+using FluentValidation;
+
+namespace dotnet_test.Validator
+{
+    public class CategoriaValidator : AbstractValidator<Categoria>
+    {
+        public CategoriaValidator()
+        {
+            RuleFor(c => c.Nome)
+                    .NotEmpty()
+                    .MinimumLength(5)
+                    .MaximumLength(100);
+        }
+    }
+}
