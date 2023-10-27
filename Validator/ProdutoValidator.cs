4e2c86da1d009afbfed2aae884dfea7432dfaa89
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-10-26 22:50:41 -0300

    Implementação da Validator para Produto

---
+++ Validator/ProdutoValidator.cs
@@ -0,0 +1,17 @@
+﻿using dotnet_test.Model;
+using FluentValidation;
+
+namespace dotnet_test.Validator
+{
+        public class ProdutoValidator : AbstractValidator<Produto>
+        {
+            public ProdutoValidator()
+            {
+                RuleFor(p => p.Nome)
+                    .NotEmpty()
+                    .MinimumLength(2)
+                    .MaximumLength(50);
+
+            }
+        }
+}
