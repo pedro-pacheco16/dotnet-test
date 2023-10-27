857449d215f1ed2b2048bbb39c919c6d88d9512e
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-10-26 23:38:29 -0300

    Criação da Model Categoria

---
+++ Model/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+
+namespace dotnet_test.Model
+{
+    public class Categoria
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long id { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(100)]
+        public string Nome { get; set; } = string.Empty;
+
+        [InverseProperty("Categoria")]
+        [JsonIgnore]
+        public virtual ICollection<Produto>? produto { get; set; }
+    }
+}
