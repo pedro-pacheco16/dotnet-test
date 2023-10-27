71b89c7db8a81255c13f3c7bc8b2a9a88c16258c
pedro-pacheco16 <pedroaugustopacheco16@gmail.com>
2023-10-26 23:11:31 -0300

    Implementação da Interface Produto

---
+++ Service/IProdutoService.cs
@@ -0,0 +1,21 @@
+﻿using dotnet_test.Model;
+
+namespace dotnet_test.Service
+{
+    public interface IProdutoService
+    {
+        Task<IEnumerable<Produto>> GetAll();
+
+        Task<Produto?> GetById(long id);
+
+        Task<IEnumerable<Produto>> GetByNome(string nome);
+
+        Task<IEnumerable<Produto>> GetByPreco(decimal preco);
+
+        Task<Produto?> Create(Produto produto);
+
+        Task<Produto?> Update(Produto produto);
+
+        Task Delete(Produto produto);
+    }
+}
